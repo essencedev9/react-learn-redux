6021b388f560f20774a9b95a5bcaee11d48f352e
essencedev9 <essencedev9@nntuple.com>
2021-08-01 17:37:55 +0900

    Add Environment - 리덕스 개발자 도구

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./modules";
+import { composeWithDevTools } from "redux-devtools-extension"; // 리덕스 개발자 도구
 
-const store = createStore(rootReducer); // 스토어를 만듭니다.
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
 
 ReactDOM.render(
   <Provider store={store}>
